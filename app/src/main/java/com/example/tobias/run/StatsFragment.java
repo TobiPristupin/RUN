e6605b11716e9f002a03846ce5ab82d694954b90
tobi <tobiaspristupin@yahoo.com>
2017-02-24 22:57:19 -0500

    Finished FragmentStats intro screen

--- app/src/main/java/com/example/tobias/run/StatsFragment.java
+++ app/src/main/java/com/example/tobias/run/StatsFragment.java
@@ -3,11 +3,7 @@ package com.example.tobias.run;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,14 +24,7 @@ public class StatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_stats, container, false);
-
-        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.stats_viewpager);
-        StatsPagerAdapter adapter = new StatsPagerAdapter(getChildFragmentManager());
-        viewPager.setAdapter(adapter);
-
-        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.stats_tab_indicator);
-        tabLayout.setupWithViewPager(viewPager, true);
-
         return rootView;
     }
+
 }
