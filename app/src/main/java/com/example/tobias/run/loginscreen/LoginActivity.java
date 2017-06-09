fb37255c5c4f6e871b24e2411cd668f960e04bfa
tobi <tobiaspristupin@yahoo.com>
2017-06-09 11:39:12 -0400

    Finished value checking when Logging in for LoginActivity.

--- app/src/main/java/com/example/tobias/run/loginscreen/LoginActivity.java
+++ app/src/main/java/com/example/tobias/run/loginscreen/LoginActivity.java
@@ -1,22 +1,40 @@
 package com.example.tobias.run.loginscreen;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.tobias.run.R;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout emailLayout;
+    private TextInputLayout passwordLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        emailLayout = (TextInputLayout) findViewById(R.id.login_email);
+        passwordLayout = (TextInputLayout) findViewById(R.id.login_password);
+
         changeStatusBarColor();
+        initLogInButton();
     }
 
+    @Override
+    public void onBackPressed() {
+        //User can't exit app by pressing back.
+    }
 
     /**
      * AppTheme status bar color attr is set to transparent for the drawerLayout in main activity.
@@ -27,5 +45,76 @@ public class LoginActivity extends AppCompatActivity {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+    }
+
+    private void initLogInButton(){
+        AppCompatButton button = (AppCompatButton) findViewById(R.id.login_button);
+
+
+        //Error resets when text is inputted
+        emailLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                emailLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        passwordLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                passwordLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailLayout.getEditText().getText().toString();
+                String password = passwordLayout.getEditText().getText().toString();
+                EmailValidator validator = EmailValidator.getInstance();
+
+                if (email.isEmpty()){
+                    emailLayout.setError("Field is required");
+                    return;
+                }
+
+                if (password.isEmpty()){
+                    passwordLayout.setError("Field is required");
+                    return;
+                }
+
+                if (!validator.isValid(email)){
+                    emailLayout.setError("Invalid email");
+                    return;
+                }
+
+                if (password.length() <= 3){
+                    passwordLayout.setError("Password must be longer than 3 characters");
+                    return;
+                }
+
+
+            }
+        });
     }
 }
