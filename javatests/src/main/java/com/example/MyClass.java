3b5511f6c7d6b69c3de943cc4b799b0a6c621660
Tobi <tobiaspristupin@yahoo.com>
2017-08-23 18:02:15 -0700

    Fixed Conversion Manager date inaccuracies

--- javatests/src/main/java/com/example/MyClass.java
+++ javatests/src/main/java/com/example/MyClass.java
@@ -1,14 +1,15 @@
 package com.example;
 
 
+import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 
 public class MyClass {
 
     public static void main(String[] args){
         LocalDate today = new LocalDate();
-        long firstDay = today.dayOfWeek().withMinimumValue().toDateTimeAtStartOfDay().getMillis() / 1000;
-        long lastDay = today.dayOfWeek().withMaximumValue().toDateTimeAtMidnight().getMillis() / 1000;
+        long firstDay = new DateTime().dayOfMonth().withMinimumValue().withHourOfDay(0).withMinuteOfHour(0).withSecondOfMinute(0).getMillis() / 1000;
+        long lastDay = new DateTime().dayOfWeek().withMaximumValue().withHourOfDay(23).withMinuteOfHour(59).withSecondOfMinute(59).getMillis() / 1000;
         System.out.println(firstDay);
         System.out.println(lastDay);
     }
