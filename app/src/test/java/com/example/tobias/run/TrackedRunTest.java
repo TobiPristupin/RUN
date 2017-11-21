218a452c47194aa53d4f6f191b3d58014362e029
Tobias Pristupin <tobipristupin@gmail.com>
2017-11-21 08:51:44 -0500

    Added equality test for Run

--- app/src/test/java/com/example/tobias/run/TrackedRunTest.java
+++ app/src/test/java/com/example/tobias/run/TrackedRunTest.java
@@ -11,12 +11,20 @@ import org.junit.Test;
 
 public class TrackedRunTest {
 
-    @Test public void testEquality(){
+    @Test public void testEquals(){
         TrackedRun trackedRun = new TrackedRun(10, 23, 45, 104, 2432, "a");
         TrackedRun trackedRun2 = new TrackedRun(10, 23, 45, 104, 2432, "a");
+        TrackedRun trackedRun3 = new TrackedRun(1, 2, 4, 1, 2, "b");
+        
         Assert.assertEquals(true, trackedRun.equals(trackedRun2));
-
-        TrackedRun trackedRun3 = new TrackedRun(10, 23, 45, 104, 2432, "b");
         Assert.assertEquals(false, trackedRun.equals(trackedRun3));
+        
+        //Reflexive
+        Assert.assertEquals(true, trackedRun.equals(trackedRun));
+        
+        //Symmetric
+        Assert.assertEquals(true, trackedRun.equals(trackedRun2) && trackedRun2.equals(trackedRun));
+       
+        Assert.assertEquals(false, trackedRun.equals(null));
     }
 }
