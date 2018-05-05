2c0a3cafc1f7b717988eaf9ae7d90b3662cba4ec
Tobi <tobipristupin@gmail.com>
2018-05-05 17:27:02 -0400

    Changed to build.gradle, updated support lib to 27.1.1

--- app/src/test/java/com/tobipristupin/simplerun/ui/editor/EditorPresenterTest.java
+++ app/src/test/java/com/tobipristupin/simplerun/ui/editor/EditorPresenterTest.java
@@ -9,8 +9,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class EditorPresenterTest {
@@ -28,51 +30,51 @@ public class EditorPresenterTest {
     @Before
     public void setup(){
         presenter = new EditorPresenter(view, runRepo, preferencesRepo);
-        Mockito.when(preferencesRepo.getDistanceUnit()).thenReturn(DistanceUnit.KM);
+        when(preferencesRepo.getDistanceUnit()).thenReturn(DistanceUnit.KM);
         //Manually set text fields equal to the run field.
-        Mockito.when(view.getDateText()).thenReturn("Mon, 4/30/2018");
-        Mockito.when(view.getDistanceText()).thenReturn("4.3 km");
-        Mockito.when(view.getRatingText()).thenReturn("3");
-        Mockito.when(view.getTimeText()).thenReturn("01:00:00");
+        when(view.getDateText()).thenReturn("Mon, 4/30/2018");
+        when(view.getDistanceText()).thenReturn("4.3 km");
+        when(view.getRatingText()).thenReturn("3");
+        when(view.getTimeText()).thenReturn("01:00:00");
     }
 
     @Test
     public void shouldSetEditModeIfNoRunWasPassed(){
         presenter.onCreateView(run);
-        Mockito.verify(view).setRatingText(String.valueOf(run.getRating()));
-        Mockito.verify(view).setActionBarEditTitle();
+        verify(view).setRatingText(String.valueOf(run.getRating()));
+        verify(view).setActionBarEditTitle();
     }
 
     @Test
     public void shouldAddToDatabaseIfCorrectData(){
         presenter.onSaveButtonPressed();
-        Mockito.verify(runRepo).add(run);
+        verify(runRepo).add(run);
     }
 
     @Test
     public void shouldNotAddToDatabaseIfInvalidData(){
         String emptyFieldFlag = "N/A";
-        Mockito.when(view.getEmptyFieldText()).thenReturn(emptyFieldFlag);
-        Mockito.when(view.getDateText()).thenReturn(emptyFieldFlag);
+        when(view.getEmptyFieldText()).thenReturn(emptyFieldFlag);
+        when(view.getDateText()).thenReturn(emptyFieldFlag);
 
         presenter.onSaveButtonPressed();
-        Mockito.verify(runRepo, Mockito.never()).add(Mockito.any());
+        verify(runRepo, never()).add(any());
     }
 
     @Test
     public void shouldUpdateIfIsInEditMode(){
         int newRating = run.getRating() + 1;
-        Mockito.when(view.getRatingText()).thenReturn(String.valueOf(newRating));
+        when(view.getRatingText()).thenReturn(String.valueOf(newRating));
         presenter.onCreateView(run);
         presenter.onSaveButtonPressed();
         run.setRating(newRating);
-        Mockito.verify(runRepo).update(run);
+        verify(runRepo).update(run);
     }
 
     @Test
     public void shouldUpdateViewsDate(){
         presenter.onDateDialogPositiveButton(2018, 5, 1);
-        Mockito.verify(view).setDateText("Tue, 5/1/2018");
+        verify(view).setDateText("Tue, 5/1/2018");
     }
 
 }
---
+++ app/src/test/java/com/tobipristupin/simplerun/ui/history/HistoryPresenterTest.java
@@ -0,0 +1,17 @@
+package com.tobipristupin.simplerun.ui.history;
+
+import com.tobipristupin.simplerun.interfaces.Observable;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.*;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class HistoryPresenterTest {
+
+
+}
